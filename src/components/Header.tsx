7e38f10ae55d2d8e92af4189f3460573c5fc3f83
syeduroojkamal <syeduroojkamal26@gmail.com>
2025-02-27 05:42:24 +0530

    fix: spelling of ramadan

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="pt-10 pb-6 flex flex-col items-center justify-center">
       <span className="text-4xl font-semibold">सेहरी - इफ़्तार</span>
       <span className="text-2xl ">चूरू समय</span>
-      <span className="text-xl ">सभी को रमजान की मुबारकबाद!</span>
+      <span className="text-xl ">सभी को रमज़ान की मुबारकबाद!</span>
       <span className="text-xl">+ AJ Medical Store +</span>
     </header>
   );
